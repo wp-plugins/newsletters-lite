15356e82be996a9f4c528cea200cdb8c69371ab4
contrid <contrid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-26 11:20:43 +0000

    Implemented LITE
    git-svn-id: https://plugins.svn.wordpress.org/newsletters-lite/trunk@973232 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/admin/lite-upgrade.php
+++ views/admin/lite-upgrade.php
@@ -18,6 +18,7 @@ require_once( ABSPATH . 'wp-admin/admin-header.php' );
 		<h3><?php _e('Upgrade to Newsletters PRO', $this -> plugin_name); ?></h3>
 		<p><?php _e('Click the button below to upgrade to Newsletters PRO', $this -> plugin_name); ?></p>
 		<p>
+			<a class="button button-secondary button-large" href="<?php echo admin_url('admin.php?page=' . $this -> sections -> submitserial); ?>" onclick="jQuery.colorbox({href:ajaxurl + '?action=<?php echo $this -> pre; ?>serialkey'}); return false;"><?php _e('Submit Serial', $this -> plugin_name); ?></a>
 			<?php $plugin_link = "http://tribulant.com/plugins/view/1/wordpress-newsletter-plugin"; ?>
 			<a class="button button-primary button-large" href="<?php echo $plugin_link; ?>" onclick="jQuery.colorbox({iframe:true, width:'90%', height:'90%', href:'<?php echo $plugin_link; ?>'}); return false;"><?php _e('Upgrade to PRO', $this -> plugin_name); ?></a>
 		</p>
